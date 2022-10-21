93638aa87bad531641e960f83477fc0d1a4c3675
intwone <cassiointw1993@gmail.com>
2022-10-21 08:55:04 -0300

    test: ensure DbAccount throws if Encrypter throws

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -12,7 +12,6 @@ const makeEncrypter = () => {
       return new Promise(resolve => resolve('hashed_password'));
     }
   }
-
   return new EncryperStub();
 };
 
@@ -37,5 +36,22 @@ describe('DbAddaccount Usecase', () => {
     await sut.add(accountData);
 
     expect(encryptSpy).toHaveBeenCalledWith('valid_password');
+  });
+
+  it('should throw an exception if Encrypter throws', async () => {
+    const { sut, encrypterStub } = makeSut();
+    jest
+      .spyOn(encrypterStub, 'encrypt')
+      .mockReturnValueOnce(
+        new Promise((resolve, reject) => reject(new Error())),
+      );
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid@mail.com',
+      password: 'valid_password',
+    };
+    const promise = sut.add(accountData);
+
+    await expect(promise).rejects.toThrow();
   });
 });
