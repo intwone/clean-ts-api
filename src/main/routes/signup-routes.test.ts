70fc8a7f2b25ebef875c83d801d1683da30b634f
intwone <cassiointw1993@gmail.com>
2022-10-24 19:02:54 -0300

    test: ensure SignUp routes returns an account on success

---
+++ src/main/routes/signup-routes.test.ts
@@ -0,0 +1,11 @@
+import request from 'supertest';
+import app from '../config/app';
+
+describe('SignUp Routes', () => {
+  it('should return an account on success', async () => {
+    await request(app)
+      .post('/api/signup')
+      .send({ name: 'any_name', email: 'any_email', password: 'any_password', passwordConfirmation: 'any_password' })
+      .expect(200);
+  });
+});
