4fe767192facd3710ebcb0e8074520acd9074561
intwone <cassiointw1993@gmail.com>
2022-10-24 18:22:50 -0300

    feat: ensure requests cors is enabled

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,6 +1,8 @@
 import { Express } from 'express';
 import { bodyParser } from '../middewares/body-parser';
+import { cors } from '../middewares/cors';
 
 export default (app: Express): void => {
   app.use(bodyParser);
+  app.use(cors);
 };
