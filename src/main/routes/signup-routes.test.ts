b19adc4c1ce92400c9e6c0310fa81b4de3e87572
intwone <cassiointw1993@gmail.com>
2022-10-25 09:01:25 -0300

    fix: add a valid email in integration test

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -19,7 +19,12 @@ describe('SignUp Routes', () => {
   it('should return an account on success', async () => {
     await request(app)
       .post('/api/signup')
-      .send({ name: 'any_name', email: 'any_email', password: 'any_password', passwordConfirmation: 'any_password' })
+      .send({
+        name: 'any_name',
+        email: 'any_email@gmail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password',
+      })
       .expect(200);
   });
 });
