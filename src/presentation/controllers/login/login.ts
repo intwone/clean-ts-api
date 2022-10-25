6df2081f8abda32b560aa77dc4d8981b9b266315
intwone <cassiointw1993@gmail.com>
2022-10-25 18:52:13 -0300

    feat: ensure LoginController returns 500 if EmailValidator throw an exception

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,5 +1,5 @@
 import { InvalidParamError, MissingParamError } from '../../errors';
-import { badRequest } from '../../helpers/http-helper';
+import { badRequest, serverError } from '../../helpers/http-helper';
 import { HttpRequestProtocol, HttpResponseProtocol } from '../../protocols';
 import { ControllerProtocol } from '../../protocols/controller';
 import { EmailValidatorProtocol } from '../signup/signup-protocols';
@@ -8,17 +8,22 @@ export class LoginController implements ControllerProtocol {
   constructor(private readonly emailValidator: EmailValidatorProtocol) {}
 
   async handle(httpRequest: HttpRequestProtocol): Promise<HttpResponseProtocol> {
-    const { email, password } = httpRequest.body;
-    if (!email) {
-      return new Promise(resolve => resolve(badRequest(new MissingParamError('email'))));
+    try {
+      const { email, password } = httpRequest.body;
+      if (!email) {
+        return new Promise(resolve => resolve(badRequest(new MissingParamError('email'))));
+      }
+      if (!password) {
+        return new Promise(resolve => resolve(badRequest(new MissingParamError('password'))));
+      }
+      const isValidEmail = this.emailValidator.isValid(email);
+      if (!isValidEmail) {
+        return new Promise(resolve => resolve(badRequest(new InvalidParamError('email'))));
+      }
+      return { statusCode: 200, body: {} };
+    } catch (error) {
+      console.error(error);
+      return serverError(error);
     }
-    if (!password) {
-      return new Promise(resolve => resolve(badRequest(new MissingParamError('password'))));
-    }
-    const isValidEmail = this.emailValidator.isValid(email);
-    if (!isValidEmail) {
-      return new Promise(resolve => resolve(badRequest(new InvalidParamError('email'))));
-    }
-    return { statusCode: 200, body: {} };
   }
 }
