dd81c92406ea3116844ff4835a35b59234d86ad6
intwone <cassiointw1993@gmail.com>
2022-10-27 09:06:07 -0300

    feat: ensure JwtAdapter returns a token when sign succeeds

--- src/infra/cryptography/jwt-adapter/jtw-adapter.ts
+++ src/infra/cryptography/jwt-adapter/jtw-adapter.ts
@@ -5,7 +5,7 @@ export class JwtAdapter implements EncrypterProtocol {
   constructor(private readonly secret: string) {}
 
   async encrypt(value: string): Promise<string> {
-    jwt.sign({ id: value }, this.secret);
-    return new Promise(resolve => resolve(''));
+    const accessToken = jwt.sign({ id: value }, this.secret);
+    return accessToken;
   }
 }
