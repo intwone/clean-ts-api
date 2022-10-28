f3f97e4005df86f13311f1667fd2a817daa52163
intwone <cassiointw1993@gmail.com>
2022-10-28 07:48:03 -0300

    feat: ensure AccountMongoRepository returns an account on loadByEmail succeeds

--- src/data/protocols/database/load-account-by-email-repository.ts
+++ src/data/protocols/database/load-account-by-email-repository.ts
@@ -1,5 +1,5 @@
 import { AccountModelProtocol } from '../../usecases/add-account/db-add-account-protocols';
 
 export interface LoadAccountByEmailRepositoryProtocol {
-  loadByEmail: (email: string) => Promise<AccountModelProtocol | null>;
+  loadByEmail: (email: string) => Promise<AccountModelProtocol>;
 }
--- src/infra/database/mongodb/account-repository/account.ts
+++ src/infra/database/mongodb/account-repository/account.ts
@@ -1,13 +1,20 @@
 import { AddAccountRepositoryProtocol } from '../../../../data/protocols/database/add-account-repository';
+import { LoadAccountByEmailRepositoryProtocol } from '../../../../data/protocols/database/load-account-by-email-repository';
 import { AccountModelProtocol } from '../../../../domain/models/account';
 import { AddAccountModelProtocol } from '../../../../domain/usecases/add-account';
 import { MongoHelper } from '../helpers/mongo-helper';
 import { map } from './account-mapper';
 
-export class AccountMongoRepository implements AddAccountRepositoryProtocol {
+export class AccountMongoRepository implements AddAccountRepositoryProtocol, LoadAccountByEmailRepositoryProtocol {
   async add(accountData: AddAccountModelProtocol): Promise<AccountModelProtocol> {
     const accountCollection = MongoHelper.getCollection('accounts');
     await accountCollection?.insertOne(accountData);
-    return map(accountData);
+    return map(accountData) as AccountModelProtocol;
+  }
+
+  async loadByEmail(email: string): Promise<AccountModelProtocol> {
+    const accountCollection = MongoHelper.getCollection('accounts');
+    const account = await accountCollection?.findOne({ email });
+    return map(account) as AccountModelProtocol;
   }
 }
