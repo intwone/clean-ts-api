70252f3cdc7c0722ec30eb0e4ea7d0f9fe791764
intwone <cassiointw1993@gmail.com>
2022-10-28 07:48:32 -0300

    test: ensure AccountMongoRepository returns an account on loadByEmail succeeds

--- src/infra/database/mongodb/account-repository/account.spec.ts
+++ src/infra/database/mongodb/account-repository/account.spec.ts
@@ -1,3 +1,4 @@
+import { Collection } from 'mongodb';
 import { MongoHelper } from '../helpers/mongo-helper';
 import { AccountMongoRepository } from './account';
 
@@ -12,9 +13,11 @@ const makeSut = (): SutProtocol => {
   };
 };
 
+let accountCollection: Collection | undefined;
+
 describe('Account Mongo Repository', () => {
   beforeEach(async () => {
-    const accountCollection = MongoHelper.getCollection('accounts');
+    accountCollection = MongoHelper.getCollection('accounts');
     await accountCollection?.deleteMany({});
   });
 
@@ -26,18 +29,34 @@ describe('Account Mongo Repository', () => {
     await MongoHelper.disconnect();
   });
 
-  it('should return an account on success', async () => {
+  it('should return an account on add success', async () => {
     const { sut } = makeSut();
     const account = await sut.add({
       name: 'any_name',
-      email: 'any_email',
+      email: 'any_email@mail.com',
       password: 'any_password',
     });
 
     expect(account).toBeTruthy();
     expect(account.id).toBeTruthy();
     expect(account.name).toBe('any_name');
-    expect(account.email).toBe('any_email');
+    expect(account.email).toBe('any_email@mail.com');
+    expect(account.password).toBe('any_password');
+  });
+
+  it('should return an account on loadByEmail success', async () => {
+    await accountCollection?.insertOne({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password',
+    });
+    const { sut } = makeSut();
+    const account = await sut.loadByEmail('any_email@mail.com');
+
+    expect(account).toBeTruthy();
+    expect(account.id).toBeTruthy();
+    expect(account.name).toBe('any_name');
+    expect(account.email).toBe('any_email@mail.com');
     expect(account.password).toBe('any_password');
   });
 });
