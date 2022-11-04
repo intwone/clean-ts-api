898bfc4eaef07f5add8b23188cb799ac9fe0ef8d
intwone <cassiointw1993@gmail.com>
2022-11-04 08:01:28 -0300

    test: ensure DbAddSurvey calls AddSurveyRepository with correct values

---
+++ src/data/usecases/add-survey/add-survey.spec.ts
@@ -0,0 +1,48 @@
+import { AddSurveyModelProtocol } from './db-add-survey-protocols';
+import { AddSurveyRepositoryProtocol } from '../../protocols/database/survey/add-survey-repository';
+import { DbAddSurvey } from './db-add-survey';
+
+interface SutProtocol {
+  sut: DbAddSurvey;
+  addSurveyRepositoryStub: AddSurveyRepositoryProtocol;
+}
+
+const makeFakeSurveyData = (): AddSurveyModelProtocol => ({
+  question: 'any_question',
+  answers: [
+    {
+      image: 'any_image',
+      answer: 'any_answer',
+    },
+  ],
+});
+
+const makeAddSurveyRepositoryStub = (): AddSurveyRepositoryProtocol => {
+  class AddSurveyRepositoryStub implements AddSurveyRepositoryProtocol {
+    async add(surveyData: AddSurveyModelProtocol): Promise<void> {
+      return new Promise(resolve => resolve());
+    }
+  }
+  const addSurveyRepositoryStub = new AddSurveyRepositoryStub();
+  return addSurveyRepositoryStub;
+};
+
+const makeSut = (): SutProtocol => {
+  const addSurveyRepositoryStub = makeAddSurveyRepositoryStub();
+  const sut = new DbAddSurvey(addSurveyRepositoryStub);
+  return {
+    sut,
+    addSurveyRepositoryStub,
+  };
+};
+
+describe('DbAddSurvey Usecase', () => {
+  it('should call AddSurveyRepository with correct values', async () => {
+    const { sut, addSurveyRepositoryStub } = makeSut();
+    const addSpy = jest.spyOn(addSurveyRepositoryStub, 'add');
+    const fakeSurveyData = makeFakeSurveyData();
+    await sut.add(fakeSurveyData);
+
+    expect(addSpy).toHaveBeenCalledWith(fakeSurveyData);
+  });
+});
