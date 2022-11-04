827587f02ed0b7fb998e0d30510df87c2ff2f287
intwone <cassiointw1993@gmail.com>
2022-11-04 08:00:45 -0300

    feat: ensure DbAddSurvey calls AddSurveyRepository with correct values

---
+++ src/data/protocols/database/survey/add-survey-repository.ts
@@ -0,0 +1,5 @@
+import { AddSurveyModelProtocol } from '../../../../domain/usecases/add-survey';
+
+export interface AddSurveyRepositoryProtocol {
+  add(surveyData: AddSurveyModelProtocol): Promise<void>;
+}
