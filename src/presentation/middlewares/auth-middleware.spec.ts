edd0058eff2c2a79f3a4b34bc21ac9f8c83a818d
intwone <cassiointw1993@gmail.com>
2022-11-07 08:39:50 -0300

    test: ensure AuthMiddleware returns 500 if LoadAccountByToken throw an exception

--- src/presentation/middlewares/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -1,7 +1,7 @@
 import { AccountModelProtocol } from '../../domain/models/account';
 import { LoadAccountByTokenProtocol } from '../../domain/usecases/load-account-by-token';
 import { AccessDeniedError } from '../errors';
-import { forbidden, success } from '../helpers/http/http-helper';
+import { forbidden, serverError, success } from '../helpers/http/http-helper';
 import { HttpRequestProtocol } from '../protocols';
 import { AuthMiddleware } from './auth-middleware';
 
@@ -70,11 +70,22 @@ describe('Auth Middleware', () => {
     expect(httpResponse).toEqual(forbidden(new AccessDeniedError()));
   });
 
-  it('should return 200 if LoadAccountByToken returns accoubt', async () => {
+  it('should return 200 if LoadAccountByToken returns account', async () => {
     const { sut } = makeSut();
     const httpRequest = makeFakeRequest();
     const httpResponse = await sut.handle(httpRequest);
 
     expect(httpResponse).toEqual(success({ accountId: 'valid_id' }));
+  });
+
+  it('should return 500 if LoadAccountByToken throw an exception', async () => {
+    const { sut, loadAccountByTokenStub } = makeSut();
+    jest
+      .spyOn(loadAccountByTokenStub, 'load')
+      .mockResolvedValueOnce(new Promise((resolve, reject) => reject(new Error())));
+    const httpRequest = makeFakeRequest();
+    const httpResponse = await sut.handle(httpRequest);
+
+    expect(httpResponse).toEqual(serverError(new Error()));
   });
 });
