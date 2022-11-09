9fb1acbafd57b6ab060343ee6040210fd37e24ac
intwone <cassiointw1993@gmail.com>
2022-11-09 19:30:51 -0300

    feat: ensure SurveyMongoRepository load all surveys

--- src/infra/database/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/database/mongodb/survey/survey-mongo-repository.ts
@@ -1,12 +1,20 @@
+import { LoadSurveysRepositoryProtocol } from '../../../../data/protocols/database/survey/load-survey-repository';
 import {
   AddSurveyModelProtocol,
   AddSurveyRepositoryProtocol,
 } from '../../../../data/usecases/add-survey/db-add-survey-protocols';
+import { SurveyModelProtocol } from '../../../../domain/models/survey';
 import { MongoHelper } from '../helpers/mongo-helper';
 
-export class SurveyMongoRepository implements AddSurveyRepositoryProtocol {
+export class SurveyMongoRepository implements AddSurveyRepositoryProtocol, LoadSurveysRepositoryProtocol {
   async add(surveyData: AddSurveyModelProtocol): Promise<void> {
     const surveyCollection = MongoHelper.getCollection('surveys');
     await surveyCollection?.insertOne(surveyData);
+  }
+
+  async loadAll(): Promise<SurveyModelProtocol[]> {
+    const surveyCollection = MongoHelper.getCollection('surveys');
+    const surveys = await surveyCollection?.find().toArray();
+    return surveys as unknown as SurveyModelProtocol[];
   }
 }
