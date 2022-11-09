8839f06704aec59ff1c089d339527b2665e88ee4
intwone <cassiointw1993@gmail.com>
2022-11-09 19:47:21 -0300

    refactor: move auth middlewares to separate files

--- src/main/routes/survey-routes.ts
+++ src/main/routes/survey-routes.ts
@@ -1,13 +1,11 @@
 import { Router } from 'express';
-import { adaptMiddleware } from '../adapters/express-middleware-adapter';
 import { adaptRoute } from '../adapters/express-route-adapter';
 import { makeAddSurveyController } from '../factories/controllers/survey/add-survey/add-survey-controller-factory';
 import { makeLoadSurveysController } from '../factories/controllers/survey/load-surveys/load-surveys-controller-factory';
-import { makeAuthMiddleware } from '../factories/middlewares/auth-middleware-factory';
+import { adminAuth } from '../middewares/admin-auth';
+import { userAuth } from '../middewares/user-auth';
 
 export default (router: Router) => {
-  const adminAuth = adaptMiddleware(makeAuthMiddleware('admin'));
-  const userAuth = adaptMiddleware(makeAuthMiddleware('user'));
   router.post('/surveys', adminAuth, adaptRoute(makeAddSurveyController()));
   router.get('/surveys', userAuth, adaptRoute(makeLoadSurveysController()));
 };
