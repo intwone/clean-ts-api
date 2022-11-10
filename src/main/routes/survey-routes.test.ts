bf75668a906497800fe3c12d9f9aaf47bfbeb0aa
intwone <cassiointw1993@gmail.com>
2022-11-10 07:52:39 -0300

    test: ensure LoadSurveysController returns 204 if return an empty array

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -1,25 +1,40 @@
 import { sign } from 'jsonwebtoken';
 import { Collection } from 'mongodb';
 import request from 'supertest';
+import { AddSurveyModelProtocol } from '../../domain/usecases/add-survey';
 import { MongoHelper } from '../../infra/database/mongodb/helpers/mongo-helper';
 import app from '../config/app';
 import env from '../config/env';
 
-const makeSurvey = () => ({
+let surveyCollection: Collection | undefined;
+let accountCollection: Collection | undefined;
+
+const makeFakeSurveyData = (): AddSurveyModelProtocol => ({
   question: 'any_question',
   answers: [
     {
-      image: 'any_image1',
+      image: 'any_image',
       answer: 'any_answer1',
     },
     {
       answer: 'any_answer2',
     },
   ],
+  date: new Date(),
 });
 
-let surveyCollection: Collection | undefined;
-let accountCollection: Collection | undefined;
+const makeAccessToken = async (): Promise<string> => {
+  const result = await accountCollection?.insertOne({
+    name: 'any_name',
+    email: 'any_email@mail.com',
+    password: 'any_password',
+    role: 'admin',
+  });
+  const id = result?.insertedId;
+  const accessToken = sign({ id }, env.jwtSecret);
+  await accountCollection?.updateOne({ _id: id }, { $set: { accessToken } });
+  return accessToken;
+};
 
 describe('Survey Routes', () => {
   beforeEach(async () => {
@@ -39,21 +54,13 @@ describe('Survey Routes', () => {
 
   describe('POST /surveys', () => {
     it('should return 403 on add survey without accessToken', async () => {
-      const fakeSurvey = makeSurvey();
+      const fakeSurvey = makeFakeSurveyData();
       await request(app).post('/api/surveys').send(fakeSurvey).expect(403);
     });
 
     it('should return 204 on add survey with valid accessToken', async () => {
-      const fakeSurvey = makeSurvey();
-      const result = await accountCollection?.insertOne({
-        name: 'any_name',
-        email: 'any_email@mail.com',
-        password: 'any_password',
-        role: 'admin',
-      });
-      const id = result?.insertedId;
-      const accessToken = sign({ id }, env.jwtSecret);
-      await accountCollection?.updateOne({ _id: id }, { $set: { accessToken } });
+      const fakeSurvey = makeFakeSurveyData();
+      const accessToken = await makeAccessToken();
       await request(app).post('/api/surveys').set('x-access-token', accessToken).send(fakeSurvey).expect(204);
     });
   });
@@ -61,6 +68,11 @@ describe('Survey Routes', () => {
   describe('GET /surveys', () => {
     it('should return 403 on load surveys without accessToken', async () => {
       await request(app).get('/api/surveys').expect(403);
+    });
+
+    it('should return 204 on load surveys with valid accessToken', async () => {
+      const accessToken = await makeAccessToken();
+      await request(app).get('/api/surveys').set('x-access-token', accessToken).expect(204);
     });
   });
 });
