01f78f316484bb6eb94e0532e48c33b4e3fb3bb6
intwone <cassiointw1993@gmail.com>
2022-11-14 09:05:12 -0300

    feat: ensure SurveyMongoRepository find survey by id with success

--- src/infra/database/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/database/mongodb/survey/survey-mongo-repository.ts
@@ -3,11 +3,14 @@ import {
   AddSurveyModelProtocol,
   AddSurveyRepositoryProtocol,
 } from '@/data/usecases/add-survey/db-add-survey-protocols';
+import { LoadSurveyByIdRepositoryProtocol } from '@/data/usecases/load-survey-by-id/db-load-survey-by-id-protocols';
 import { SurveyModelProtocol } from '@/domain/models/survey';
 import { MongoHelper } from '@/infra/database/mongodb/helpers/mongo-helper';
-import { map } from './survey-mapper';
+import { map } from './surveys-mapper';
 
-export class SurveyMongoRepository implements AddSurveyRepositoryProtocol, LoadSurveysRepositoryProtocol {
+export class SurveyMongoRepository
+  implements AddSurveyRepositoryProtocol, LoadSurveysRepositoryProtocol, LoadSurveyByIdRepositoryProtocol
+{
   async add(surveyData: AddSurveyModelProtocol): Promise<void> {
     const surveyCollection = MongoHelper.getCollection('surveys');
     await surveyCollection?.insertOne(surveyData);
@@ -17,5 +20,11 @@ export class SurveyMongoRepository implements AddSurveyRepositoryProtocol, LoadS
     const surveyCollection = MongoHelper.getCollection('surveys');
     const surveys = await surveyCollection?.find().toArray();
     return map(surveys);
+  }
+
+  async loadById(id: string): Promise<SurveyModelProtocol> {
+    const surveyCollection = MongoHelper.getCollection('surveys');
+    const survey = await surveyCollection?.findOne({ _id: id });
+    return map(survey);
   }
 }
--- src/infra/database/mongodb/survey/surveys-mapper.ts
+++ src/infra/database/mongodb/survey/surveys-mapper.ts
@@ -1,7 +1,9 @@
-import { SurveyModelProtocol } from '@/domain/models/survey';
-
-export const map = (surveys: any): SurveyModelProtocol[] => {
+export const map = (surveys: any): any => {
   if (!surveys) return [];
+  if (!Array.isArray(surveys)) {
+    const { _id, ...surveysWithoutId } = surveys;
+    return { ...surveysWithoutId, id: _id };
+  }
   const mappedSurveys = surveys.map((survey: any) => {
     const { _id, ...surveysWithoutId } = survey;
     return { ...surveysWithoutId, id: _id };
