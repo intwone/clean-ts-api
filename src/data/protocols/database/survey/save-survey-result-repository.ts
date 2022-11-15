94745bfc63514af6c4d3fba70bc0fe5dffc88fd1
intwone <cassiointw1993@gmail.com>
2022-11-15 07:12:22 -0300

    feat: ensure SurveyResultMongoRepository insert new surveyResult

--- src/data/protocols/database/survey/save-survey-result-repository.ts
+++ src/data/protocols/database/survey/save-survey-result-repository.ts
@@ -2,5 +2,5 @@ import { SurveyResultModelProtocol } from '@/domain/models/survey-result';
 import { SaveSurveyResultModelProtocol } from '@/domain/usecases/save-survey-result';
 
 export interface SaveSurveyResultRepositoryProtocol {
-  save(data: SaveSurveyResultModelProtocol): Promise<SurveyResultModelProtocol>;
+  save: (data: SaveSurveyResultModelProtocol) => Promise<SurveyResultModelProtocol>;
 }
