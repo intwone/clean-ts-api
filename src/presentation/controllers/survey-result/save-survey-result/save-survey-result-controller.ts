a8c2c6eb120f5b875d548cffe1b10742e2d66c76
intwone <cassiointw1993@gmail.com>
2022-11-17 08:02:10 -0300

    feat: ensure SaveSurveyResultController calls SaveSurveyResult with correct values

--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
@@ -5,15 +5,20 @@ import {
   HttpRequestProtocol,
   HttpResponseProtocol,
   LoadSurveyByIdProtocol,
+  SaveSurveyResultProtocol,
 } from './save-survey-result-controller-protocols';
 
 export class SaveSurveyResultController implements ControllerProtocol {
-  constructor(private readonly loadSurveyById: LoadSurveyByIdProtocol) {}
+  constructor(
+    private readonly loadSurveyById: LoadSurveyByIdProtocol,
+    private readonly saveSurveyResult: SaveSurveyResultProtocol,
+  ) {}
 
   async handle(httpRequest: HttpRequestProtocol): Promise<HttpResponseProtocol> {
     try {
       const { surveyId } = httpRequest.params;
       const { answer } = httpRequest.body;
+      const { accountId } = httpRequest;
       const survey = await this.loadSurveyById.loadById(surveyId);
       if (!survey) {
         return forbidden(new InvalidParamError('surveyId'));
@@ -22,6 +27,12 @@ export class SaveSurveyResultController implements ControllerProtocol {
       if (!answers.includes(answer)) {
         return forbidden(new InvalidParamError('answer'));
       }
+      await this.saveSurveyResult.save({
+        accountId: accountId as string,
+        surveyId,
+        answer,
+        date: new Date(),
+      });
       return null as unknown as HttpResponseProtocol;
     } catch (error) {
       return serverError(error);
--- src/presentation/protocols/http.ts
+++ src/presentation/protocols/http.ts
@@ -7,4 +7,5 @@ export interface HttpRequestProtocol {
   body?: any;
   headers?: any;
   params?: any;
+  accountId?: string;
 }
