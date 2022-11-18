2ed841dae0862720ebbf5758be46e04e5d003184
intwone <cassiointw1993@gmail.com>
2022-11-18 18:48:24 -0300

    feat: add swagger api

---
+++ src/main/docs/index.ts
@@ -0,0 +1,8 @@
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: 'Clean Node API',
+    description: 'Api desenvolvida para realizar enquetes entre desenvolvedores de software.',
+    version: '1.0.0',
+  },
+};
